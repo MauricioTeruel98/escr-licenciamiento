8c51f91a029f4b620cdcd70e5c89aec1a122de5e
MauricioTeruel98 <mauricioteruel98@gmail.com>
2024-11-19 14:07:46 -0300

    (changes) - cambios en el layout

--- resources/js/Layouts/ImageLayout.jsx
+++ resources/js/Layouts/ImageLayout.jsx
@@ -6,28 +6,32 @@ export default function ImageLayout({ children, title }) {
             <Head title={title} />
             <div className="min-h-screen grid md:grid-cols-3">
                 {/* Left side - Form */}
-            <div className="p-8 flex flex-col items-center justify-center md:col-span-2">
-                <Head title={title} />
+                <div className="p-8 flex flex-col md:relative md:col-span-2">
+                    <Head title={title} />
 
-                <div className="mb-12">
-                    <img
-                        src="/assets/img/logo_esc.png"
-                        alt="Costa Rica Logo"
-                        className="h-10"
-                    />
+                    {/* Logo - static in mobile, absolute in desktop */}
+                    <div className="mb-8 md:mb-0 md:absolute md:top-8 md:left-8">
+                        <img
+                            src="/assets/img/logo_esc.png"
+                            alt="Costa Rica Logo"
+                            className="h-10"
+                        />
+                    </div>
+
+                    {/* Content */}
+                    <div className="md:flex-1 md:flex md:items-center md:justify-center">
+                        {children}
+                    </div>
                 </div>
 
-                {children}
-            </div>
-
-            {/* Right side - Image */}
-            <div className="hidden md:block md:col-span-1">
-                <img
-                    src="/assets/img/bg_register.png"
-                    alt="Costa Rica Landscape"
-                    className="w-full h-full object-cover"
-                />
-            </div>
+                {/* Right side - Image */}
+                <div className="hidden md:block md:col-span-1">
+                    <img
+                        src="/assets/img/bg_register.png"
+                        alt="Costa Rica Landscape"
+                        className="w-full h-full object-cover"
+                    />
+                </div>
             </div>
         </>
     );
--- resources/js/Layouts/InstructionsLayout.jsx
+++ resources/js/Layouts/InstructionsLayout.jsx
@@ -6,10 +6,11 @@ export default function InstructionsLayout({ children, title }) {
             <Head title={title} />
             <div className="min-h-screen grid md:grid-cols-3">
                 {/* Left side - Form */}
-                <div className="p-8 flex flex-col items-center justify-center md:col-span-2">
+                <div className="p-8 flex flex-col md:relative md:col-span-2">
                     <Head title={title} />
 
-                    <div className="mb-12">
+                    {/* Logo - static in mobile, absolute in desktop */}
+                    <div className="mb-8 md:mb-0 md:absolute md:top-8 md:left-8">
                         <img
                             src="/assets/img/logo_esc.png"
                             alt="Costa Rica Logo"
@@ -17,7 +18,10 @@ export default function InstructionsLayout({ children, title }) {
                         />
                     </div>
 
-                    {children}
+                    {/* Content */}
+                    <div className="md:flex-1 md:flex md:items-center md:justify-center">
+                        {children}
+                    </div>
                 </div>
 
                 {/* Right side - Instructions */}
